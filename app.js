e5937141905127d26e265c26721c82bd93ffeba5
ma1VAR3 <prajjwalgupta1465@gmail.com>
2021-07-10 11:10:11 +0530

    node app init

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongo yeahh")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+require('./models/user')
+
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
+
